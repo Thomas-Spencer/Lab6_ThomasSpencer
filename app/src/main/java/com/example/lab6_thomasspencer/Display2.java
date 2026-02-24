ec43865df842c86809a96cc42e127fa3a79e41db
Clay1999 <122058495+Clay1999@users.noreply.github.com>
2026-02-24 13:07:55 -0800

    Update Display2.java

--- app/src/main/java/com/example/lab6_thomasspencer/Display2.java
+++ app/src/main/java/com/example/lab6_thomasspencer/Display2.java
@@ -4,9 +4,9 @@ package com.example.lab6_thomasspencer;
 public class Display2{
 
     public String display(){
-        // adding my comment here
+        // My comment added inside GitHub
         return "Clay";
 
-        // adding another comment in Andriod studio
+        
     }
 }
