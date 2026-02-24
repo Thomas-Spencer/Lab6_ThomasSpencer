6727be330915296b829ca155e4ef1bbba9649e38
JennyHalbert <jennykatehalbert@gmail.com>
2026-02-24 10:07:01 -0800

    adding comments in AS

--- app/src/main/java/com/example/lab6_thomasspencer/Display.java
+++ app/src/main/java/com/example/lab6_thomasspencer/Display.java
@@ -6,5 +6,7 @@ package com.example.lab6_thomasspencer;
     public String display(){
         // adding my comment here
         return "Jenny";
+
+        // adding another comment in Andriod studio
     }
 }
